c7a5fa40eaf808d03d069bc24d957e7b84229102
l15k4 <liska.jakub@gmail.com>
2017-08-02 21:12:56 +0200

    removing accepted tasks for simplicity

--- src/core/src/main/scala/gwi/mawex/State.scala
+++ src/core/src/main/scala/gwi/mawex/State.scala
@@ -1,11 +1,10 @@
 package gwi.mawex
 
-import scala.collection.immutable.Queue
 import scala.collection.breakOut
 
 object State {
 
-  def empty: State = State(pendingTasks = Queue.empty, tasksInProgress = Map.empty, acceptedTaskIds = Map.empty, doneTaskIds = Set.empty)
+  def empty: State = State(pendingTasks = Map.empty, tasksInProgress = Map.empty, doneTaskIds = Set.empty)
 
   trait TaskDomainEvent
   case class TaskAccepted(task: Task) extends TaskDomainEvent
@@ -17,30 +16,30 @@ object State {
 
 // TODO do something with State#doneTaskIds, the set will grow indefinitely which is not good for a use case with millions of tasks
 
-case class State private(private val pendingTasks: Queue[Task], private val tasksInProgress: Map[TaskId, Task], private val acceptedTaskIds: Map[TaskId, Long], private val doneTaskIds: Set[TaskId]) {
+case class State private(private val pendingTasks: Map[Task, Long], private val tasksInProgress: Map[TaskId, Task], private val doneTaskIds: Set[TaskId]) {
   import State._
 
-  def getPendingTasks: List[Task] = pendingTasks.toList
-  def getPendingConsumerGroups: Set[String] = pendingTasks.map(_.id.consumerGroup)(breakOut)
-  def getAcceptedTasks: Map[TaskId, Long] = acceptedTaskIds
-  def isAccepted(taskId: TaskId): Boolean = acceptedTaskIds.contains(taskId)
+  def getPendingTasks: Set[Task] = pendingTasks.keySet
+  def getPendingConsumerGroups: Set[String] = pendingTasks.map(_._1.id.consumerGroup)(breakOut)
+  def getAcceptedTasks: Map[TaskId, Long] = pendingTasks.map { case (task, ts) => task.id -> ts }
+  def isAccepted(taskId: TaskId): Boolean = pendingTasks.exists(_._1.id == taskId)
   def getTaskInProgress(taskId: TaskId): Option[Task] = tasksInProgress.get(taskId)
   def isDone(taskId: TaskId): Boolean = doneTaskIds.contains(taskId)
 
   def updated(event: TaskDomainEvent): State = event match {
     case TaskAccepted(task) =>
-      copy(pendingTasks = pendingTasks enqueue task, acceptedTaskIds = acceptedTaskIds.updated(task.id, System.currentTimeMillis))
+      copy(pendingTasks = pendingTasks.updated(task, System.currentTimeMillis))
 
     case TaskStarted(taskId) =>
-      val (task, rest) = pendingTasks.partition(_.id == taskId)
-      require(task.size == 1)
-      copy(pendingTasks = rest, tasksInProgress = tasksInProgress + (taskId -> task.head))
+      val task = pendingTasks.find(_._1.id == taskId).map(_._1).getOrElse(throw new IllegalStateException("Very unexpectedly, accepted task is missing !!!"))
+      val rest = pendingTasks.filter(_._1.id != taskId)
+      copy(pendingTasks = rest, tasksInProgress = tasksInProgress + (task.id -> task))
 
     case TaskCompleted(taskId, _) =>
-      copy(tasksInProgress = tasksInProgress - taskId, acceptedTaskIds = acceptedTaskIds - taskId, doneTaskIds = doneTaskIds + taskId)
+      copy(tasksInProgress = tasksInProgress - taskId, doneTaskIds = doneTaskIds + taskId)
 
     case WorkerFailed(taskId) =>
-      copy(tasksInProgress = tasksInProgress - taskId, acceptedTaskIds = acceptedTaskIds - taskId, doneTaskIds = doneTaskIds + taskId)
+      copy(tasksInProgress = tasksInProgress - taskId, doneTaskIds = doneTaskIds + taskId)
 
   }
 }

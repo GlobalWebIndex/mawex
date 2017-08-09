0b2c0593564eedc79ab9a5c0ecffb5b351a93130
l15k4 <liska.jakub@gmail.com>
2017-08-10 01:09:32 +0200

    improving traits

--- src/api/src/main/scala/gwi/mawex/OpenProtocol.scala
+++ src/api/src/main/scala/gwi/mawex/OpenProtocol.scala
@@ -2,31 +2,30 @@ package gwi.mawex
 
 import scala.util.Try
 
-sealed trait TaskLike
-
 /**
   *  client => proxy =>
   *                  Master <=> Workers
   *  client <= proxy <=
   */
-case class TaskId(id: String, consumerGroup: String) extends TaskLike
-case class Task(id: TaskId, job: Any) extends TaskLike
-case class TaskResult(task: Task, result: Either[String, Any]) extends TaskLike
+case class TaskId(id: String, consumerGroup: String)
+case class Task(id: TaskId, job: Any)
+case class TaskResult(task: Task, result: Either[String, Any])
 
 /** Executor => Worker */
 object e2w {
-  case class TaskExecuted(result: Try[Any]) extends TaskLike
+  case class TaskExecuted(result: Try[Any])
 }
 
 /** proxy <= master */
 object m2p {
-  case class TaskAck(taskId: TaskId) extends TaskLike
+  case class TaskAck(taskId: TaskId)
 }
 
 /** client <= proxy */
 object p2c {
-  case class Accepted(taskId: TaskId) extends TaskLike
-  case class Rejected(taskId: TaskId) extends TaskLike
+  sealed trait TaskSubmission
+  case class Accepted(taskId: TaskId) extends TaskSubmission
+  case class Rejected(taskId: TaskId) extends TaskSubmission
 }
 
 

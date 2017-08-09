b097f41feec09469b68cce1f857174def0862340
l15k4 <liska.jakub@gmail.com>
2017-08-10 01:09:06 +0200

    there needs to be way to obtain Master state in tests

--- src/core/src/main/scala/gwi/mawex/InternalProtocol.scala
+++ src/core/src/main/scala/gwi/mawex/InternalProtocol.scala
@@ -2,21 +2,24 @@ package gwi.mawex
 
 import java.util.UUID
 
-sealed trait Worker2MasterCommand
-sealed trait Master2WorkerCommand
-
 /** Master <=> Worker */
 case class WorkerId(consumerGroup: String, pod: String, id: String = UUID.randomUUID().toString)
 
 /** Workers => Master */
-object w2m {
+protected[mawex] object w2m {
+  sealed trait Worker2MasterCommand
   case class CheckIn(workerId: WorkerId) extends Worker2MasterCommand
   case class CheckOut(workerId: WorkerId) extends Worker2MasterCommand
   case class TaskRequest(workerId: WorkerId) extends Worker2MasterCommand
   case class TaskFinished(workerId: WorkerId, taskId: TaskId, result: Either[String, Any]) extends Worker2MasterCommand
 }
 /** Master => Workers */
-object m2w {
+protected[mawex] object m2w {
+  sealed trait Master2WorkerCommand
   case object TaskReady extends Master2WorkerCommand
   case class TaskResultAck(taskId: TaskId) extends Master2WorkerCommand
 }
+
+/** Testing */
+protected[mawex] case object GetMawexState
+protected[mawex] case class MawexState(workState: State, workersById: Map[WorkerId, WorkerRef])

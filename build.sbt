211ba09fea2c281248f1e2e9b7016c3ae9a1a843
l15k4 <liska.jakub@gmail.com>
2018-11-21 11:33:06 +0100

    making cluster work with newest akka

--- build.sbt
+++ build.sbt
@@ -5,10 +5,11 @@ lazy val s3Resolver = "S3 Snapshots" at "s3://public.maven.globalwebindex.net.s3
 
 crossScalaVersions in ThisBuild := Seq("2.12.6", "2.11.8")
 organization in ThisBuild := "net.globalwebindex"
-libraryDependencies in ThisBuild ++= clist ++ loggingApi
+libraryDependencies in ThisBuild ++= clist ++ loggingApi ++ Seq(akkaActor, akkaClusterTools)
 resolvers in ThisBuild ++= Seq(
   "Maven Central Google Mirror EU" at "https://maven-central-eu.storage-download.googleapis.com/repos/central/data/",
   Resolver.bintrayRepo("tanukkii007", "maven"),
+  "dnvriend" at "http://dl.bintray.com/dnvriend/maven",
   s3Resolver
 )
 version in ThisBuild ~= (_.replace('+', '-'))
@@ -19,19 +20,18 @@ stage in (ThisBuild, Docker) := null
 
 lazy val `mawex-api` = (project in file("src/api"))
   .settings(publishSettings("globalWebIndex", "mawex-api", s3Resolver))
-  .settings(libraryDependencies ++= Seq(akkaActor, akkaClusterTools))
 
 lazy val `mawex-core` = (project in file("src/core"))
   .enablePlugins(DockerPlugin, SmallerDockerPlugin, JavaAppPackaging)
   .settings(fork in Test := true)
   .settings(libraryDependencies ++=
-    Seq(loggingImplLogback, akkaCluster, akkaPersistence, akkaKryoSerialization, fabric8JavaClient, k8sJavaClient, akkaClusterCustomDowning, akkaPersistenceInMemory, akkaTestkit, scalatest)
+    Seq(loggingImplLogback, akkaCluster, akkaPersistence, akkaPersistenceQuery, akkaKryoSerialization, fabric8JavaClient, k8sJavaClient, akkaClusterCustomDowning, akkaPersistenceInMemory, akkaTestkit, scalatest)
   ).settings(publishSettings("globalWebIndex", "mawex-core", s3Resolver))
   .settings(Deploy.settings("gwiq", "mawex-core", "gwi.mawex.Launcher"))
   .dependsOn(`mawex-api` % "compile->compile;test->test")
 
 lazy val `mawex-example` = (project in file("src/example"))
   .enablePlugins(DockerPlugin, SmallerDockerPlugin, JavaAppPackaging)
-  .settings(libraryDependencies ++= Seq(loggingImplLogback))
+  .settings(libraryDependencies ++= Seq(akkaCluster, akkaPersistence, akkaPersistenceRedis, akkaPersistenceDynamoDB, loggingImplLogback))
   .settings(Deploy.settings("gwiq", "mawex-example", "gwi.mawex.ExampleLauncher"))
   .dependsOn(`mawex-core` % "compile->compile;test->test")
--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
 
-  val akkaVersion                       = "2.5.16"
+  val akkaVersion                       = "2.5.18"
 
   lazy val clist                        = Seq(
     "org.backuity.clist"            %%    "clist-core"                                % "3.5.0",
@@ -18,9 +18,12 @@ object Dependencies {
   lazy val akkaActor                    = "com.typesafe.akka"             %%    "akka-actor"                                % akkaVersion
   lazy val akkaCluster                  = "com.typesafe.akka"             %%    "akka-cluster"                              % akkaVersion
   lazy val akkaClusterTools             = "com.typesafe.akka"             %%    "akka-cluster-tools"                        % akkaVersion
-  lazy val akkaClusterCustomDowning     = "com.github.TanUkkii007"        %%    "akka-cluster-custom-downing"               % "0.0.9"
+  lazy val akkaClusterCustomDowning     = "com.github.TanUkkii007"        %%    "akka-cluster-custom-downing"               % "0.0.12"
   lazy val akkaPersistence              = "com.typesafe.akka"             %%    "akka-persistence"                          % akkaVersion
-  lazy val akkaPersistenceInMemory      = "com.github.dnvriend"           %%    "akka-persistence-inmemory"                 % "2.5.1.1"               % "test"
+  lazy val akkaPersistenceQuery         = "com.typesafe.akka"             %%    "akka-persistence-query"                    % akkaVersion
+  lazy val akkaPersistenceInMemory      = "com.github.dnvriend"           %%    "akka-persistence-inmemory"                 % "2.5.15.1"               % "test"
+  lazy val akkaPersistenceRedis         = "com.safety-data"               %%    "akka-persistence-redis"                    % "0.4.1"
+  lazy val akkaPersistenceDynamoDB      = "com.typesafe.akka"             %%    "akka-persistence-dynamodb"                 % "1.1.1"
   lazy val akkaKryoSerialization        = "net.globalwebindex"            %%    "akka-kryo-serialization"                   % "0.5.3-SNAPSHOT" // adhoc published before PR is merged https://github.com/romix/akka-kryo-serialization/pull/124
   lazy val akkaSlf4j                    = "com.typesafe.akka"             %%    "akka-slf4j"                                % akkaVersion
   lazy val akkaTestkit                  = "com.typesafe.akka"             %%    "akka-testkit"                              % akkaVersion             % "test"
